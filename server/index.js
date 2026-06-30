8eb2a87198d2c9b5d28f14d5f123d095db5397b3
Daksh Chikara <dakshchikara35@gmail.com>
2026-06-30 17:31:40 +0530

    sixth-commit

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express();
 
 // 1. CORS MUST GO FIRST
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://note-mind-io-client.onrender.com',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
