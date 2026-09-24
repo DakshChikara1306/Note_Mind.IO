683617a73ba69c5896491f923ed56bf3f2c3b4ea
Daksh Chikara <dakshchikara35@gmail.com>
2026-09-24 18:39:24 +0530

    bug-fixes

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express();
 
 // 1. CORS MUST GO FIRST
 app.use(cors({
-    origin: 'https://note-mind-io-client.onrender.com',
+    origin: process.env.CLIENT_URL,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
